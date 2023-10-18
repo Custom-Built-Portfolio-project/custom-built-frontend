277b0c7a977f0e806ae72fdfc71a155c661a3d41
Alan Otero <Oteroalan06@gmail.com>
2023-10-18 13:15:25 -0300

    realice la validacion de telefono dependiendo el codigo de area del pais

--- src/components/navBar/Contacto/Validation.tsx
+++ src/components/navBar/Contacto/Validation.tsx
@@ -1,3 +1,21 @@
+export interface CountryDigitsMap {
+  [key: string]: number;
+}
+
+export const allowedDigitsByCountry: CountryDigitsMap = {
+  '52': 10, // México tiene 10 dígitos (incluyendo el código de área)
+  '54': 10, // Argentina tiene 10 dígitos (incluyendo el código de área)
+  '55': 11, // Brasil tiene 11 dígitos (incluyendo el código de área)
+  '56': 9,  // Chile tiene 9 dígitos (incluyendo el código de área)
+  '57': 10, // Colombia tiene 10 dígitos (incluyendo el código de área)
+  '58': 10, // Venezuela tiene 10 dígitos (incluyendo el código de área)
+  '51': 9,  // Perú tiene 9 dígitos (incluyendo el código de área)
+  '34': 9,  // España tiene 9 dígitos (incluyendo el código de área)
+  '1': 10   // Estados Unidos y Canadá tienen 10 dígitos (incluyendo el código de área)
+};
+
+
+
 export const validateName = (name: string): string | undefined => {
   const nameRegex = /^[A-Z][a-zA-Z0-9]*$/;
   if (!name.match(nameRegex)) {
@@ -6,15 +24,20 @@ export const validateName = (name: string): string | undefined => {
   return undefined;
 };
 
-export const validatePhone = (phone: string): string | undefined => {
-  // Expresión regular que permite números de teléfono de celular y teléfono fijo en diferentes formatos
-  const phoneRegex = /^(?:(?:\+|0{0,2})\d{1,3})?[-.\s]?\(?\d{2,3}\)?[-.\s]?\d{3,4}[-.\s]?\d{3,4}$/;
+export const validatePhone = (phone: string, countryCode: string): string | undefined => {
+  // Obtiene la cantidad de dígitos permitidos para el país seleccionado
+  const allowedDigits = allowedDigitsByCountry[countryCode];
+
+  // Expresión regular que valida un código de área de 2 o 3 dígitos seguido de un número de teléfono con la cantidad permitida de dígitos
+  const phoneRegex = new RegExp(`^[2-9]{1}[0-9]{1}\\s\\d{${allowedDigits}}$`);
 
   if (!phone.match(phoneRegex)) {
     return 'Número de teléfono no válido.';
   }
   return undefined;
 };
+
+
 
 export const validateEmail = (email: string): string | undefined => {
   // Expresión regular para validar una dirección de correo electrónico
