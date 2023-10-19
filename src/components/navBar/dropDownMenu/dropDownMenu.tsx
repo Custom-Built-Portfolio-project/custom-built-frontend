c524a23bab2382cf0ccdd4b001549d754c8b8f90
ptsuarez91@hotmail.com <ptsuarez91@hotmail.com>
2023-10-19 13:37:17 -0300

    cambios en la navBar pendiente logica de submenu

--- src/components/navBar/dropDownMenu/dropDownMenu.tsx
+++ src/components/navBar/dropDownMenu/dropDownMenu.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
-import './dropDownMenu.css'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import './dropDownMenu.css';
+
+interface DropdownSubMenuItem {
+  text: string;
+  path: string;
+}
 
 interface DropdownMenuItem {
   text: string;
   path: string;
+  submenu?: DropdownSubMenuItem[];
 }
 
 interface DropdownMenuGroup {
@@ -20,18 +26,45 @@ interface DropdownMenuProps {
 
 const DropdownMenu: React.FC<DropdownMenuProps> = ({ groups }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [openSubmenu, setOpenSubmenu] = useState<number | null>(null);
 
+  const [categoriaIsOpen, setCategoriaIsOpen] = useState<boolean>(false);
+  const [openMenuCategoriesSubMenu, setOpenMenuCategoriesSubMenu] = useState<number | null>(null);
+  const [categoriesMenuSUB , setCategoriesMenuSUB] = useState<number | null> (null)
+
+  const seteoDePosicionEnGroup=(setNumber)=>{
+    setCategoriesMenuSUB(setNumber)
+  }
+  console.log(categoriaIsOpen)
+  console.log(openMenuCategoriesSubMenu)
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const toggleSubmenu = (groupIndex: number | null) => {
+    if (openSubmenu === groupIndex) {
+      setOpenSubmenu(null);
+    } else {
+      setOpenSubmenu(groupIndex);
+    }
+  };
+
+  const toggleCategoriaSubMenu = () => {
+    setCategoriaIsOpen(!categoriaIsOpen);
+  };
+
+  const toggleMenuCategoriesSubMenu = (submenuIndex: number | null) => {
+    setOpenMenuCategoriesSubMenu(submenuIndex);
   };
 
   return (
     <div className='dropdown-menu'>
       <div className='dropdown-menu-header' onClick={toggleMenu}>
-        <span>Explora</span>
         <FontAwesomeIcon
-          icon={faAngleDown}
-          style={{ transform: `rotate(${isOpen ? 180 : 0}deg)` }}
+          icon={faBars}
+          beat
+          size="lg"
+          style={{ cursor: 'pointer' }}
         />
       </div>
       {isOpen && (
@@ -39,17 +72,48 @@ const DropdownMenu: React.FC<DropdownMenuProps> = ({ groups }) => {
           <ul className='dropdown-menu-list'>
             {groups.map((group, groupIndex) => (
               <React.Fragment key={groupIndex}>
-                <li className='dropdown-menu-divider'>{group.title}</li>
-                {group.items.map((item, index) => (
-                  <li key={index}>
-                    <Link to={item.path}>{item.text}</Link>
-                  </li>
-                ))}
+                <li
+                  className='dropdown-menu-divider'
+                  onClick={() => toggleSubmenu(groupIndex)}
+                >
+                  {group.title}
+                </li>
+                {openSubmenu === groupIndex && (
+                  <ul>
+                    {group.items.map((item, index) => (
+                      <li key={index}
+                        onMouseEnter={() => {toggleCategoriaSubMenu(); toggleMenuCategoriesSubMenu(index); seteoDePosicionEnGroup(index) }} >   {/*aca es cuando estoy parado en el item se cambia el estado setcategoriaIsopen  a true  * */}
+                        <Link to={item.path}>
+                          {item.text}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
               </React.Fragment>
             ))}
           </ul>
         </div>
       )}
+      {categoriaIsOpen && (
+        <React.Fragment>
+          <div className='categoria-submenu'>
+            <ul className='categoria-submenu-list'>
+              {/* Aquí puedes mapear los elementos del submenú de categorías */}
+              {groups[categoriesMenuSUB].items[openMenuCategoriesSubMenu].submenu?.map((subitem, index) => (
+                <li
+                  key={index}
+
+                  className={openMenuCategoriesSubMenu === index ? 'active' : ''}
+                >
+                  <Link to={subitem.path}>{subitem.text}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </React.Fragment>
+      )}
+
     </div>
   );
 };
