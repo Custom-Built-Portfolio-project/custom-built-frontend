b880ba634f8d8a0e18da557bff255e789f96ddf4
ptsuarez91@hotmail.com <ptsuarez91@hotmail.com>
2023-10-20 14:09:11 -0300

    termine la logica de la searchBar

--- src/components/navBar/SearchNavBar/SearchNavBar.tsx
+++ src/components/navBar/SearchNavBar/SearchNavBar.tsx
@@ -1,39 +1,65 @@
 import React, { useEffect, useState } from 'react';
-
+import axios from 'axios';
+import SearchResultsDropdown from './dropDownMenuSearchBar';
+import "./searchNavBar.css"
 
 
 const SearchNavBar: React.FC = () => {
 
-    const [searchText, setSearchText] = useState<string>("");
+    const [searchText, setSearchText] = useState<string>(""); //texto donde se guarda las palabras de resultados 
 
     const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchText(event.target.value)
     }
 
-    const verResultadoBusqueda = async (searchText : string) => {
-      try {
-        const {data} = axios.get("llamada a la api")
-        
-      } catch (error) {
-        const messageErrorBusqueda= "No se han encontrado resultados relacionados con la busqueda" ;
-      }
+
+    const [searchResults, setSearchResults] = useState([]); // se guardo las objetos que vienen por resultado con su propiedades 
+    const [showResults, setShowResults] = useState(false); //dice si abre el formulario o no 
+    const [searchError, setSearchError] = useState<string | null>(null); // Estado para mensajes de error
+
+    const verResultadoBusqueda = async (searchText: string) => {
+        try {
+            const { data } = await axios.get(`llamada a la api ${searchText}`)
+            if (data.length > 0) {
+                setSearchResults(data)
+                setShowResults(true)
+            }
+            else {
+                setSearchResults([])
+                setShowResults(true)
+                setSearchError("No se han encontrado resultados. Por favor, inténtalo de nuevo.")
+            }
+        } catch (error) {
+            console.error("Error en la búsqueda:", error);
+            setShowResults(true)
+            setSearchError("error en la busqueda")
+        }
 
     }
 
     useEffect(() => {
-        verResultadoBusqueda(searchText)
-    },
-        [searchText])
+        if (searchText.trim() === "") {
+            // Si no hay texto de búsqueda, no hacemos la llamada a la API y ocultamos los resultados.
+            setShowResults(false);
+            setSearchError(null);
+        } else {
+            verResultadoBusqueda(searchText);
+        }
+    }, [searchText]);
 
-    return (
-        <div>
-            <input
+        return (
+            <div className="search-container">
+              <input
                 type="text"
                 value={searchText}
                 onChange={handleSearchChange}
-                placeholder='Buscar...' />
-        </div>
-    );
+                placeholder='Buscar...'
+              />
+              <div className="search-results-container">
+                <SearchResultsDropdown results={searchResults} isOpen={showResults} error={searchError} />
+              </div>
+            </div>
+          );
 };
 
 export default SearchNavBar;
